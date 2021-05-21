31be25f2ad83dbf71e21a7572ad66aa06469ffb0
kirypto <kirypto@gmail.com>
2021-05-21 15:54:58 -0600

    Step2 * Add a /api/hello/<name> route to allow greeting anyone via a provided name

--- Source/Python/api.py
+++ Source/Python/api.py
@@ -5,3 +5,7 @@ def register_endpoints(flask_app: Flask) -> None:
     @flask_app.route("/api/hello", methods=["GET"])
     def api_hello__get():
         return make_response("Hello, Flask API!", 200)
+
+    @flask_app.route("/api/hello/<name>", methods=["GET"])
+    def api_hello_name__get(name: str):
+        return make_response(f"Hello, {name}!", 200)
