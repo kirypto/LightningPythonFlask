a8a0632373d821ad989d2f0c4b1e8e0d41cf4cd2
kirypto <kirypto@gmail.com>
2021-05-21 17:16:35 -0600

    Step2 * Add two /api/greetings routes for retrieving all greetings or a specific language's greeting

--- Source/Python/api.py
+++ Source/Python/api.py
@@ -1,4 +1,13 @@
+from json import dumps
+
 from flask import Flask, make_response, request
+
+_GREETINGS_BY_LANGUAGE = {
+    "english": "Hello",
+    "spanish": "Hola",
+    "german": "Hallo",
+    "latin": "Salve",
+}
 
 
 def register_endpoints(flask_app: Flask) -> None:
@@ -8,14 +17,24 @@ def register_endpoints(flask_app: Flask) -> None:
         greeting = _get_greeting_in(language)
         return make_response(f"{greeting}, {name}!", 200)
 
+    @flask_app.route("/api/greetings", methods=["GET"])
+    def api_greetings__get():
+        response = make_response(dumps(_GREETINGS_BY_LANGUAGE), 200)
+        response.content_type = "application/json"
+        return response
+
+    @flask_app.route("/api/greeting/<language>", methods=["GET"])
+    def api_greeting_language__get(language: str):
+        try:
+            greeting = _get_greeting_in(language)
+            response = make_response(greeting, 200)
+        except ValueError as e:
+            response = make_response(dumps({"error": str(e)}), 404)
+        response.content_type = "application/json"
+        return response
+
 
 def _get_greeting_in(language: str) -> str:
-    greetings_by_language = {
-        "english": "Hello",
-        "spanish": "Hola",
-        "german": "Hallo",
-        "latin": "Salve",
-    }
-    if language not in greetings_by_language:
+    if language not in _GREETINGS_BY_LANGUAGE:
         raise ValueError(f"No greeting is registered for desired language '{language}'")
-    return greetings_by_language[language]
+    return _GREETINGS_BY_LANGUAGE[language]
