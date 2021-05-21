9f89e0673c9eaa619cde21f2be2714aa6c40f721
kirypto <kirypto@gmail.com>
2021-05-21 15:27:58 -0600

    Step2 * Specifically set methods param of /api/hello to 'GET'

--- Source/Python/api.py
+++ Source/Python/api.py
@@ -2,6 +2,6 @@ from flask import Flask, make_response
 
 
 def register_endpoints(flask_app: Flask) -> None:
-    @flask_app.route("/api/hello")
+    @flask_app.route("/api/hello", methods=["GET"])
     def api_hello__get():
         return make_response("Hello, Flask API!", 200)
