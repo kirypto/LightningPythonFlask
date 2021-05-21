b2113182cc9d3caad801f5b242b0d3f007ab5b69
kirypto <kirypto@gmail.com>
2021-05-21 16:27:38 -0600

    Step2 * Modify /api/hello/<name> route support changing the greeting via providing 'language' query param

--- Source/Python/api.py
+++ Source/Python/api.py
@@ -1,7 +1,21 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 
 
 def register_endpoints(flask_app: Flask) -> None:
     @flask_app.route("/api/hello/<name>", methods=["GET"])
     def api_hello_name__get(name: str):
-        return make_response(f"Hello, {name}!", 200)
+        language = request.args.get("language", type=str) if "language" in request.args else "english"
+        greeting = _get_greeting_in(language)
+        return make_response(f"{greeting}, {name}!", 200)
+
+
+def _get_greeting_in(language: str) -> str:
+    greetings_by_language = {
+        "english": "Hello",
+        "spanish": "Hola",
+        "german": "Hallo",
+        "latin": "Salve",
+    }
+    if language not in greetings_by_language:
+        raise ValueError(f"No greeting is registered for desired language '{language}'")
+    return greetings_by_language[language]
