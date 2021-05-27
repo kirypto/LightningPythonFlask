2068c19197bf8ed16f08ec7a03a7771ecc45202e
kirypto <kirypto@gmail.com>
2021-05-27 15:14:10 -0600

    Step4 * Combine each set of 2 routes into a single multi-method handler

--- Source/Python/api.py
+++ Source/Python/api.py
@@ -13,49 +13,38 @@ _GREETINGS_BY_LANGUAGE = {
 def register_endpoints(flask_app: Flask) -> None:
     @flask_app.route("/api/hello/<name>", methods=["GET"])
     def api_hello_name__get(name: str):
-        language = request.args.get("language", type=str) if "language" in request.args else "english"
-        greeting = _get_greeting_in(language)
+        language = (request.args.get("language", type=str)
+                    if "language" in request.args else "english")
+        greeting = _GREETINGS_BY_LANGUAGE[language]
         return make_response(f"{greeting}, {name}!", 200)
 
-    @flask_app.route("/api/greetings", methods=["GET"])
-    def api_greetings__get():
-        response = make_response(dumps(_GREETINGS_BY_LANGUAGE), 200)
+    @flask_app.route("/api/greetings", methods=["GET", "PUT"])
+    def api_greetings():
+        if request.method == "GET":
+            response = make_response(dumps(_GREETINGS_BY_LANGUAGE), 200)
+        else:
+            greetings: dict = request.json
+            if type(greetings) is not dict or any(map(
+                    lambda x: type(x) is not str,
+                    list(greetings.keys()) + list(greetings.values()))):
+                raise ValueError("Body contents must be a dictionary of strings")
+            _GREETINGS_BY_LANGUAGE.clear()
+            _GREETINGS_BY_LANGUAGE.update(greetings)
+            response = make_response(dumps(_GREETINGS_BY_LANGUAGE), 200)
         response.content_type = "application/json"
         return response
 
-    @flask_app.route("/api/greetings", methods=["PUT"])
-    def api_greetings__put():
-        greetings: dict = request.json
-        if type(greetings) is not dict or \
-                any(map(lambda x: type(x) is not str, list(greetings.keys()) + list(greetings.values()))):
-            raise ValueError("Body contents must be a dictionary of strings")
-        _GREETINGS_BY_LANGUAGE.clear()
-        _GREETINGS_BY_LANGUAGE.update(greetings)
-        response = make_response(dumps(_GREETINGS_BY_LANGUAGE), 200)
-        response.content_type = "application/json"
-        return response
-
-    @flask_app.route("/api/greeting/<language>", methods=["GET"])
-    def api_greeting_language__get(language: str):
-        try:
-            greeting = _get_greeting_in(language)
+    @flask_app.route("/api/greeting/<language>", methods=["GET", "PUT"])
+    def api_greeting_language(language: str):
+        if request.method == "GET":
+            greeting = _GREETINGS_BY_LANGUAGE[language]
             response = make_response(greeting, 200)
-        except ValueError as e:
-            response = make_response(dumps({"error": str(e)}), 404)
+        else:
+            is_new_greeting = language in _GREETINGS_BY_LANGUAGE
+            greeting = request.data.decode("utf8")
+            _GREETINGS_BY_LANGUAGE[language] = greeting
+            response = make_response(
+                f"Set greeting for {language} to be '{greeting}'",
+                201 if is_new_greeting else 200)
         response.content_type = "application/json"
         return response
-
-    @flask_app.route("/api/greeting/<language>", methods=["PUT"])
-    def api_greeting_language__put(language: str):
-        is_new_greeting = language in _GREETINGS_BY_LANGUAGE
-        greeting = request.data.decode("utf8")
-        _GREETINGS_BY_LANGUAGE[language] = greeting
-        response = make_response(f"Set greeting for {language} to be '{greeting}'", 201 if is_new_greeting else 200)
-        response.content_type = "application/json"
-        return response
-
-
-def _get_greeting_in(language: str) -> str:
-    if language not in _GREETINGS_BY_LANGUAGE:
-        raise ValueError(f"No greeting is registered for desired language '{language}'")
-    return _GREETINGS_BY_LANGUAGE[language]
