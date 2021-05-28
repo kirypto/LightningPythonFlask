bd87003ff6505899f7c90b480651a28172ca422d
kirypto <kirypto@gmail.com>
2021-05-27 19:54:02 -0600

    Step4 * Add exception handling to all route handlers

--- Source/Python/api.py
+++ Source/Python/api.py
@@ -1,6 +1,8 @@
 from json import dumps
+from typing import Callable, Union
 
 from flask import Flask, make_response, request
+from werkzeug.exceptions import BadRequest
 
 _GREETINGS_BY_LANGUAGE = {
     "english": "Hello",
@@ -12,6 +14,7 @@ _GREETINGS_BY_LANGUAGE = {
 
 def register_endpoints(flask_app: Flask) -> None:
     @flask_app.route("/api/hello/<name>", methods=["GET"])
+    @with_error_response_on_raised_exceptions
     def api_hello_name__get(name: str):
         language = (request.args.get("language", type=str)
                     if "language" in request.args else "english")
@@ -19,6 +22,7 @@ def register_endpoints(flask_app: Flask) -> None:
         return make_response(f"{greeting}, {name}!", 200)
 
     @flask_app.route("/api/greetings", methods=["GET", "PUT"])
+    @with_error_response_on_raised_exceptions
     def api_greetings():
         if request.method == "GET":
             response = make_response(dumps(_GREETINGS_BY_LANGUAGE), 200)
@@ -35,6 +39,7 @@ def register_endpoints(flask_app: Flask) -> None:
         return response
 
     @flask_app.route("/api/greeting/<language>", methods=["GET", "PUT"])
+    @with_error_response_on_raised_exceptions
     def api_greeting_language(language: str):
         if request.method == "GET":
             greeting = _GREETINGS_BY_LANGUAGE[language]
@@ -48,3 +53,27 @@ def register_endpoints(flask_app: Flask) -> None:
                 201 if is_new_greeting else 200)
         response.content_type = "application/json"
         return response
+
+
+def _error_response(ex: Union[BaseException, str], status_code: int):
+    error_message = ex if type(ex) is str else f"{type(ex).__name__}: {ex}"
+    response = make_response(dumps({"error": error_message, "status_code": status_code}), status_code)
+    response.content_type = "application/json"
+    return response
+
+
+def with_error_response_on_raised_exceptions(handler_function: Callable) -> Callable:
+    def inner(*args, **kwargs):
+        try:
+            return handler_function(*args, **kwargs)
+        except (TypeError, ValueError, AttributeError) as e:
+            return _error_response(e, 400)
+        except BadRequest as e:
+            return _error_response(str(e), 400)
+        except KeyError as e:
+            return _error_response(e, 404)
+        except BaseException as e:
+            return _error_response(e, 500)
+
+    inner.__name__ = f"__wrapped__{handler_function.__name__}"  # Flask needs method names to be unique
+    return inner
